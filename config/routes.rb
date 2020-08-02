37cb15d8598ce94346143e5044a45b7f5a8f648d
Yuki Harada <y.harada59@gmail.com>
2020-08-02 22:56:20 +0900

    add has_secure_password

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'login', to: 'session#create'
+  
   root 'home#index'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
